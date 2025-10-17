2b9fb3ce8926d6e648730544493b54f0321bfe58
Sean Erick C. Ramones <seancramones@gmail.com>
2025-10-18 03:07:44 +0800

    feat: Add content preview API configuration in nuxt.config.ts for Nuxt Studio

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,6 +19,12 @@ export default defineNuxtConfig({
 
   compatibilityDate: '2024-11-01',
 
+  content: {
+    preview: {
+      api: 'https://api.nuxt.studio'
+    }
+  },
+
   nitro: {
     prerender: {
       routes: [
