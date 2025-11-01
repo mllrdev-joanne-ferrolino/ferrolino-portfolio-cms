f9ee059208d0455448e9fe6edee36c02b6ad28d9
Sean Erick C. Ramones <seancramones@gmail.com>
2025-11-02 03:19:51 +0800

    feat: update app configuration by removing profile picture; revise blog and project descriptions for clarity and focus

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,10 +1,5 @@
 export default defineAppConfig({
   global: {
-    picture: {
-      dark: 'https://images.unsplash.com/photo-1701615004837-40d8573b6652?q=80&w=1480&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      light: 'https://images.unsplash.com/photo-1701615004837-40d8573b6652?q=80&w=1480&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      alt: 'My profile picture'
-    },
     meetingLink: 'https://calendly.com/seancramones/15-min-quick-chat',
     email: 'seancramones@gmail.com',
     available: true
